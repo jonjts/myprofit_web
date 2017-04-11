52410bf4c04fd9db4c8a3392e528f266b11ffb0a
Jonas <jonjts@gmail.com>
2017-04-11 09:51:03 -0300

    Ajustes nos links de troca de idioma.

--- app/controllers/language_controller.rb
+++ app/controllers/language_controller.rb
@@ -1,12 +1,23 @@
-class LanguageController < ApplicationController
+class LanguageController < ActionController::Base
+
 
   def set_pt
     session[:language] = "pt-BR"
-    redirect_to root_path locale: "pt-BR"
+   redirect
   end
 
   def set_en
     session[:language] = "en"
-    redirect_to root_path locale: "en"
+    redirect
   end
 end
+
+private 
+
+def redirect
+	 if current_user.present?
+    	redirect_to root_path
+    else	
+    	redirect_to new_session_path :user
+    end
+	end
