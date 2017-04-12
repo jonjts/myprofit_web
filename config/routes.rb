4e72d8874d6f24c74159a4cef5ce4715b3ee8455
Jonas Tavares <jonjts@gmail.com>
2017-04-12 17:04:50 -0300

    Teste no servidor.

--- config/routes.rb
+++ config/routes.rb
@@ -8,16 +8,15 @@ Rails.application.routes.draw do
       sessions:       "users/sessions",
       passwords:      "users/passwords",
     }, :path_names => {:sign_in => "login", :sign_out => "logout"}, skip: :omniauth_callbacks
+
+    resources :bills
+    root to: 'home#index'
   end
 
   devise_for :users, only: :omniauth_callbacks, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
-
-  resources :bills
 
   resources :language do
     post :set_pt, :on => :collection
     post :set_en, :on => :collection
   end
-
-  root to: 'home#index', locale: /en|pt-BR/
 end
